e5084279f89bf473b1ba7ff6e953c33c64f4e294
MickeyMicky <mtchawa21@gmail.com>
2023-08-04 17:26:53 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
+
+int check_num(char *str);
 
 /**
  * main - program that adds positive numbers
@@ -13,47 +16,57 @@
 
 int main(int argc, char *argv[])
 {
+	int count;
+	int str_to_int;
 	int sum = 0;
-	int i, j, num;
 
-	if (argc == 1)
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		printf("0\n");
-		return (0);
-	}
+		if (check_num(argv[count]))
 
-	for (i = 1; i < argc; i++)
-	{
-		const char *arg = argv[i];
-		bool is_valid = true;
-
-		for (j = 0; arg[j] != '\0'; j++)
 		{
-			if (!isdigit(arg[j]))
-			{
-				is_valid = false;
-				break;
-			}
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
 
-		if (is_valid)
-		{
-			num = atoi(arg);
-			if (num > 0)
-			{
-				sum += num;
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		} else
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+
+		count++;
 	}
+
 	printf("%d\n", sum);
+
 	return (0);
+}
+
+/**
+ * check_num - function that string to a digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
 }
