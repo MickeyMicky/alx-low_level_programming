e0ed85c649dff81249aa01ebe06bf9ac93347411
MickeyMicky <mtchawa21@gmail.com>
2023-08-14 11:31:58 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog's basic info
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-typedef struct dog dog_t;
 
 #endif
