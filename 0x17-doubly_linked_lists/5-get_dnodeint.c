608ddfa6babcc9eb75a70c158e8f575d0ce25e3e
MickeyMicky <mtchawa21@gmail.com>
2023-11-09 12:14:53 -0800

    3 - 8 DLL

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_dnodeint_at_index - function that returns the nth
  * node of a dlistint_t linked list.
- * @head: pointer to head 
+ * @head: pointer to head
  * @index: index of node to find
  *
  * Return: pointer to node if found else NULL
