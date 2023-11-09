d30ac32bcd9f60ddb94a2a1ba97737863ffffb86
MickeyMicky <mtchawa21@gmail.com>
2023-11-09 06:00:58 -0800

    0 to 3 DLL

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+#include <stdlib.h>
+
+dlistint_t *create_node(const int n);
+
+/**
+ * add_dnodeint - Function that adds a node to the head
+ *                of a doubly linked list
+ * @head: double pointer to the head
+ * @n: data
+ *
+ * Return: pointer to new element else NULL on failure.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *n_node = NULL;
+	dlistint_t *temp = NULL;
+
+	n_node = create_node(n);
+	if (!n_node)
+		return (NULL);
+
+	if (!head || !(*head)) /* NULL DLL */
+	{
+		*head = n_node;
+		return (n_node);
+	}
+	else /* DLL exists */
+	{
+		temp = *head;
+		temp->prev = n_node;
+		*head = n_node;
+		n_node->next = temp;
+		return (n_node);
+	}
+
+	return (NULL);
+}
+
+/**
+ * create_node - function that creates a new DLL node with data
+ * @n: data to add to node
+ *
+ * Return: pointer to new node
+ */
+dlistint_t *create_node(const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+
+	return (new_node);
+}
